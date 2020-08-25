291f5f3f6f5e5177590746cad5185cee80982b2d
vandermnt <vanderson.mantovani@hotmail.com>
2020-08-24 23:43:38 -0300

    gostack

---
+++ src/pages/Favorites/index.tsx
@@ -0,0 +1,88 @@
+import { useNavigation } from '@react-navigation/native';
+import React, { useCallback, useEffect, useState } from 'react';
+import { Image } from 'react-native';
+
+import api from '../../services/api';
+import formatValue from '../../utils/formatValue';
+
+import {
+  Container,
+  Header,
+  HeaderTitle,
+  FoodsContainer,
+  FoodList,
+  Food,
+  FoodImageContainer,
+  FoodContent,
+  FoodTitle,
+  FoodDescription,
+  FoodPricing,
+} from './styles';
+
+interface Food {
+  id: number;
+  name: string;
+  description: string;
+  price: number;
+  thumbnail_url: string;
+  formattedPrice: string;
+}
+
+const Favorites: React.FC = () => {
+  const [favorites, setFavorites] = useState<Food[]>([]);
+
+  const navigation = useNavigation();
+
+  useEffect(() => {
+    async function loadFavorites(): Promise<void> {
+      const response = await api.get<Food[]>('/favorites');
+
+      const newFavoritesList = response.data.map(favorite => {
+        return Object.assign(favorite, {
+          formattedPrice: formatValue(favorite.price),
+        });
+      });
+
+      setFavorites(newFavoritesList);
+    }
+
+    loadFavorites();
+  }, []);
+
+  async function handleNavigate(id: number): Promise<void> {
+    // Navigate do ProductDetails page
+    navigation.navigate('FoodDetails', { id });
+  }
+
+  return (
+    <Container>
+      <Header>
+        <HeaderTitle>Meus favoritos</HeaderTitle>
+      </Header>
+
+      <FoodsContainer>
+        <FoodList
+          data={favorites}
+          keyExtractor={item => String(item.id)}
+          renderItem={({ item }) => (
+            <Food activeOpacity={0.6} onPress={() => handleNavigate(item.id)}>
+              <FoodImageContainer>
+                <Image
+                  style={{ width: 88, height: 88 }}
+                  source={{ uri: item.thumbnail_url }}
+                />
+              </FoodImageContainer>
+              <FoodContent>
+                <FoodTitle>{item.name}</FoodTitle>
+                <FoodDescription>{item.description}</FoodDescription>
+                <FoodPricing>{item.formattedPrice}</FoodPricing>
+              </FoodContent>
+            </Food>
+          )}
+        />
+      </FoodsContainer>
+    </Container>
+  );
+};
+
+export default Favorites;
